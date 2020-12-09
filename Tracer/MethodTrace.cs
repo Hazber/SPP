3661c59966f0b00f46659c13176bd25d35061d62
Hazber <62963127+Hazber@users.noreply.github.com>
2020-12-09 12:32:29 +0300

    Tracer.dll

---
+++ Tracer/MethodTrace.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace Tracer
+{
+    public class MethodTrace
+    {
+        public List<MethodTrace> _nestedStack=new List<MethodTrace>();
+        private Stopwatch _stopwatch=new Stopwatch();
+
+       public string name { get; }
+       public string classname { get; }
+       public long time { get; set; }
+
+        internal MethodTrace(MethodBase mth)
+        {
+            name = mth.Name;
+            classname = mth.DeclaringType.Name;
+            _stopwatch.Start();
+        }
+
+        internal void StopTrace()
+        {
+            _stopwatch.Stop();
+            time = _stopwatch.ElapsedMilliseconds;
+        }
+
+        internal void NewNestedMethod(MethodTrace methodTraceInfo)
+        {
+            _nestedStack.Add(methodTraceInfo);
+        }
+    }
+}
