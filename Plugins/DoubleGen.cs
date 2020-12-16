e26a68c6e063493fd86d693a03c510c04f00b9e0
Hazber <62963127+Hazber@users.noreply.github.com>
2020-12-16 11:13:08 +0300

    Plugins+Tests

---
+++ Plugins/DoubleGen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Faker;
+
+namespace Plugins
+{
+    public class DoubleGen: IGenerator
+	{
+		public Type[] PossibleTypes => new Type[] { typeof(double) };
+		private const int minPow = -323, maxPow = 309;
+		private Random _numGen;
+
+		public DoubleGen()
+		{
+			_numGen = new Random();
+		}
+
+		public DoubleGen(Random numGen)
+		{
+			_numGen = numGen;
+		}
+
+		public object Generate(Type type)
+		{
+			if (!PossibleTypes.Contains(type))
+				throw new ArgumentException();
+
+			double exponent = Math.Pow(10.0, _numGen.Next(minPow, maxPow));
+			return _numGen.NextDouble() * exponent;
+		}
+	}
+}
---
+++ Plugins/LongGen.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Faker;
+
+namespace Plugins
+{
+    class LongGen: IGenerator
+    {
+		public Type[] PossibleTypes => new[] { typeof(long) };
+		private const int minValue = -2147483648, maxValue = 2147483647;
+		private Random _numGen;
+
+		public LongGen()
+		{
+			_numGen = new Random();
+		}
+
+		public LongGen(Random numGen)
+		{
+			_numGen = numGen ?? throw new ArgumentNullException();
+		}
+
+		public object Generate(Type type)
+		{
+			if (!PossibleTypes.Contains(type))
+				throw new ArgumentException();
+
+			long result = (long)_numGen.Next(minValue, maxValue) * _numGen.Next();
+
+			if (result == 0)
+				result = 1;
+
+			return result;
+		}
+	}
+}
